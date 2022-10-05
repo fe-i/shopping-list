4ee38a0b63aa5b140d67ba12393d8c52b2e09692
fe-i <feic47@gmail.com>
2022-10-05 12:28:26 -0400

    test

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
       <h1>Shopping List</h1>
 
       <ItemCard name="pencil" quantity={2} price={0.51}/>
-      <ItemCard name="pen" quantity={3} price={1.23}/>
-      <ItemCard name="highlighter" quantity={1} price={2.11}/>
+      <ItemCard name="pens" quantity={3} price={1.23}/>
+      <ItemCard name="highlighters" quantity={1} price={2.11}/>
     </div>
   );
 }
