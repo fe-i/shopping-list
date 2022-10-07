c2fc2dca93f8686a0bf3bd84110e7cf122045d98
fe-i <feic47@gmail.com>
2022-10-07 19:42:49 -0400

    qwseeeeeeeeeee

--- src/components/inputModal/inputModal.js
+++ src/components/inputModal/inputModal.js
@@ -5,13 +5,20 @@ const InputModal = () => {
     return (
         <div>
             <form onSubmit={(e) => {
-                e.preventDefault()
-                console.log(e.target[0].value)
-            }
-            }>
-                <input type="text" id="fname" name="fname" required/>
-                <input />
-                <input />
+                e.preventDefault();
+                console.log(e.target[0].value);
+                console.log(e.target[1].value);
+                console.log(e.target[2].value);
+            }}>
+                <label>Name: </label>
+                <input type="text" name="name" required value="1"/>
+                <br />
+                <label>Quantity: </label>
+                <input type="number" name="quantity" required value="1"/>
+                <br />
+                <label>Price: $</label>
+                <input type="text" name="price" required value="1"/>
+                <br />
                 <button type="submit" />
             </form>
         </div>
