80feb3d4bef7d5afef054c57859c07bca6e59f85
fe-i <feic47@gmail.com>
2022-10-09 10:38:03 -0400

    FGUIEBVEW

--- src/components/itemCard/itemCard.js
+++ src/components/itemCard/itemCard.js
@@ -3,9 +3,12 @@ import "./itemCard.css";
 function ItemCard({ name, quantity, price }) {
   return (
     <div className="itemCard">
-      <p className="itemName">{name}</p>
-      <p>Quantity: {quantity}</p>
-      <p>Price: ${price}</p>
+      <div>
+        <p className="itemName">{name}</p>
+        <p>Quantity: {quantity}</p>
+        <p>Price: ${price}</p>
+      </div>
+      <button className="removeButton">-</button>
     </div>
   );
 }
