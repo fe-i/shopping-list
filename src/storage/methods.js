1e01c470b443801dc62511cd7972e189c96d0be9
fe-i <feic47@gmail.com>
2022-10-27 12:33:24 -0400

    dwdsds

---
+++ src/storage/methods.js
@@ -0,0 +1,16 @@
+export const getCards = () => {
+    return localStorage.getItem("cards") == null ? [] : JSON.parse(localStorage.getItem("cards"));
+}
+
+export const addCard = (newCard) => {
+    return getCards() == []
+    ? localStorage.setItem("cards", JSON.stringify([newCard]))
+    : localStorage.setItem("cards", JSON.stringify([...getCards(), newCard]));
+}
+
+export const deleteCard = (id) => {
+    const cards = getCards();
+    const newCards = cards.filter(c => c.id != id);
+}
+
+export const id = () => (new Date()).getTime();
